b3623dd791cd4342cea6dd85160133b687b41bb8
TobiasBms <tobi8678@edu.ucl.dk>
2020-03-04 16:19:22 +0100

    Created a standard socket

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+server.listen(PORT);
+
+app.get("/", function(req,res){
+    res.send("Hello world");    
+});
+
+io.on("connection", function(socket){
+    console.log("Made a connection", socket.id);
+    socket.on("chat", function(data){
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on("data", function(data){
+        io.emit("test", data);
+    });
+}); 
---
+++ todos.md
@@ -0,0 +1,15 @@
+## Overview for tasks of the backend
+
+### Server setup
+ *[ ] Setup some type of server
+ *[ ] Setup mongodb
+ *[ ] Connect to database
+### Folder Structure
+ *[ ] Find out what type of structure we should do
+ *[ ] files folder
+### Endpoints
+ *[ ] Create a standard Route group
+ *[ ] build out 
+### Sockets
+ *[ ] Create a socket for drawing board
+### Authentication
