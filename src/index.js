1208f0628f9797c88e0efd8f0f684b21e33e84fc
TobiasBms <tobi8678@edu.ucl.dk>
2020-03-05 15:22:09 +0100

    Knex Added

--- src/index.js
+++ src/index.js
@@ -3,24 +3,39 @@ const PORT = process.env.PORT;
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const {Model} = require("objection");
+const knex = require("knex")({
+    client: "mysql",
+    connection: {
+        host: "127.0.0.1",
+        user: "admin",
+        password: "admin",
+        database: "testdb"
+    }
+});
+
+Model.knex(knex);   
 
 server.listen(PORT);
+
+
 
 app.get("/", function(req,res){
     res.send("Hello world");    
 });
 
 io.on("connection", function(socket){
-    console.log("Made a connection", socket.id);
+
     socket.on("chat", function(data){
         io.sockets.emit("chat", data);
     });
 
+    socket.on("graph", function(data){
+        console.log(data);
+        io.sockets.emit("graph", data);
+    })
+    
     socket.on("data", function(data){
         io.emit("test", data);
     });
-}); 
-
-io.on("disconnect", function(socket){
-    console.log("disconnected");
 });
