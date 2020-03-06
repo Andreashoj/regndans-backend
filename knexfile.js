11851f6ac78ea9aae84f5b4a9052fc853a4b20a3
Kevin Mangkornkaew <Kevin-mangkornkaew@hotmail.com>
2020-03-06 20:46:19 +0100

    set debug to true, to enable debugging for all queries

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const path = require("path");
 module.exports = {
-  client: process.env.CLIENT,
-  connection: {
-      host: process.env.HOST,
-      user: process.env.USER,
-      password: process.env.PASS,
-      database: process.env.DB
-  },
-  migrations: {
-    directory: path.resolve("./src/migrations"),
-  },
+    client: process.env.CLIENT,
+    connection: {
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: process.env.DB
+    },
+    migrations: {
+        directory: path.resolve("./src/migrations"),
+    },
+    debug: true
 };
