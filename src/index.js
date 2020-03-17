6f6de29be3465533b06e818efec1835d9cd066db
Kevin Mangkornkaew <kem@morningtrain.dk>
2020-03-17 22:35:39 +0100

    added Authentication [WIP]

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const config = require("../knexfile");
 const knex = require("knex")(config);
 const {Model} = require("objection");
@@ -6,8 +7,15 @@ const app = require("express")();
 const server = require("http").Server(app);
 const bodyParser = require("body-parser");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const io = require("socket.io")(server);
 const {format} = require('date-fns');
+
+if(!process.env.jwtPrivate) {
+    console.log('FATAL ERROR: jwtPrivateKey is not defined.')
+    process.exit(1);
+}
+
 
 //Intacing the model with the knex sql.
 Model.knex(knex);
@@ -17,6 +25,7 @@ server.listen(PORT);
 //Middlwares
 app.use(bodyParser.json());
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 
 io.on("connection", function (socket) {
