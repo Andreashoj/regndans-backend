a8708ab20d4cf96ee09ebb5fe4ed4c856a91b400
Kevin Mangkornkaew <kem@morningtrain.dk>
2020-03-22 00:33:19 +0100

    Getting current user Route

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,20 +4,20 @@ const User = require("../models/Person");
 const auth = require('../middleware/auth');
 
 
-async function getUser(id) {
-    //We use the try catch here because the await blocks the thread.
-    try {
-        //Objection provides multiple ways to work with the database, Here is a simple query example.
-        const person = await User.query().findById(id);
-        //Date fns makes javascript dates easy to work with here is an example of a simple format.
-        if (typeof person.created === "object") {
-            const test = format(person.created, 'yyyy-MM-dd');
-        }
-        return person;
-    } catch (e) {
-        console.error(e);
-    }
-}
+// async function getUser(id) {
+//     //We use the try catch here because the await blocks the thread.
+//     try {
+//         //Objection provides multiple ways to work with the database, Here is a simple query example.
+//         const person = await User.query().findById(id);
+//         //Date fns makes javascript dates easy to work with here is an example of a simple format.
+//         if (typeof person.created === "object") {
+//             const test = format(person.created, 'yyyy-MM-dd');
+//         }
+//         return person;
+//     } catch (e) {
+//         console.error(e);
+//     }
+// }
 
 async function createUser(newUser) {
     try {
@@ -30,10 +30,17 @@ async function createUser(newUser) {
     }
 }
 
-router.get("/:id", auth, async function (req, res) {
-    const person = await getUser(req.params.id);
-    res.status(200).json({data: person});
-});
+// router.get("/:id", auth, async function (req, res) {
+//     const person = await getUser(req.params.id);
+//     res.status(200).json({data: person});
+// });
+
+router.get("/me", auth, async function (req, res) {
+    console.log('this is the req id', req.user)
+    const user = await User.query().findById(req.user._id).select('id', 'username', 'email');
+    console.log(user)
+    res.send(user)
+})
 
 router.post("/", async function (req, res) {
     const user = await createUser(req.body)
